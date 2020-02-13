64a855dd0d3875472fc3966a44143cf8d7c8d3c0
andble <andreasbleichner@gmail.com>
2020-02-13 11:21:26 +0100

    Added order to dispatcher, project completed.

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,21 +1,3 @@
-//-----------------HTML-delen för itemMenu--------------
-                    /*<div v-for="burger in burgers">
-                        <h2>{{ burger.name }}</h2>
-                        <img v-bind:src="burger.src" width="200px" height="121px">
-                        <ul>
-                            <li>
-                                {{"kCal: " + burger.kCal}}
-                            </li>
-                            <li v-if="burger.lact || burger.glut"> 
-                                {{"Innehåller laktos och gluten"}}
-                            </li>
-                            <li>
-                                {{burger.comment}}
-                            </li>
-                        </ul>
-                    </div v-for="burger in burgers">*/
-
-//------------------Vue delen--------------
  function getGender() {
     let ele = document.getElementsByName('gender');
   
@@ -78,7 +60,7 @@ const socket = io();
                   this.localOrder.y = event.clientY - 10 - offset.y;
         },
         addOrder: function() {
-            this.fullname = "Name: " + document.getElementById("name").value;
+            this.fullname = "Namn: " + document.getElementById("name").value;
             this.email = "E-mail: " + document.getElementById("email").value;
             var pay = document.getElementById("payment");
             this.payment = "Betalningsmetod: " + pay.options[pay.selectedIndex].value;
@@ -100,28 +82,3 @@ const socket = io();
         },
     }
 });
-//---------------------------loop och conditional------------------------
-  // I HTML-filen: 
-  /*
-   <div id="myID">
-    <h1> {{arbitraryVariableName}} </h1>
-
-      <ul>
-       <li v-for="burger in burgers">
-        {{ burger.display() }}
-       </li>
-      </ul>
-</div>*/
-
-//I Vue:
-/*
-<div id="myID">
-    <h1> {{arbitraryVariableName}} </h1>
-
-      <ul>
-       <li v-for="burger in burgers">
-      {{ burger.display()}} <br /> <span v-if="burger.glut"> Contains gluten  </span> <br /> <span v-if="burger.lact"> Contains lactose </span>
-       </li>
-      </ul>
-</div>
-  })*/
